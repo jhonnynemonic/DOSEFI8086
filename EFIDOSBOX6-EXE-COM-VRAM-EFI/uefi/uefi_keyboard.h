5bc3386ba9c7ed7a5d44ba5e1d249e10d684a2df
jhonnynemonic <112034026+jhonnynemonic@users.noreply.github.com>
2026-07-08 21:45:45 +0200

    Delete EFIDOSBOX6-EXE-COM-VRAM-EFI directory

--- EFIDOSBOX6-EXE-COM-VRAM-EFI/uefi/uefi_keyboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <efi.h>
-
-void keyboard_init(EFI_SYSTEM_TABLE *SystemTable);
-void keyboard_poll();
-void dos_keyboard_buffer_push(uint8_t ascii);
-uint8_t dos_keyboard_buffer_pop();
-extern int last_key;
-extern EFI_SYSTEM_TABLE *KbdST;
